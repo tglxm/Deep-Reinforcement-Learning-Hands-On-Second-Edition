ed7b2edbd1f53c815d218a7c3773fd5cd97e06b3
tglxm <2633680461@qq.com>
2022-06-04 14:35:56 +0800

    修改原因：修改环境测试程序 修改人：tglxm

--- Chapter02/02_cartpole_random.py
+++ Chapter02/02_cartpole_random.py
@@ -3,7 +3,7 @@ import gym
 
 if __name__ == "__main__":
     env = gym.make("CartPole-v0")
-    env = gym.wrappers.Monitor(env, "recording")
+    env = gym.wrappers.Monitor(env, "recording", force=True)
 
     total_reward = 0.0
     total_steps = 0
--- Chapter02/test.py
+++ Chapter02/test.py
@@ -1,8 +1,11 @@
 import gym
-env = gym.make('SpaceInvaders-v0')
-env.reset()
-for i in range(1000):
-	env.render()
-	o,r,d,i = env.step(env.action_space.sample())
-	if d:
-		env.reset()
+
+
+if __name__ == "__main__":
+	env = gym.make('SpaceInvaders-v0')
+	env.reset()
+	for i in range(1000):
+		env.render()
+		o, r, d, i = env.step(env.action_space.sample())
+		if d:
+			env.reset()
